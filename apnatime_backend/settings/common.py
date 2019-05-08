62876eee8ec282d1cef4457bd2e8ee757ead55e2
Harshit Garg <harshitgargmnit@gmail.com>
2019-05-08 09:31:19 +0530

    Add whitenoise for staticassests

--- apnatime_backend/settings/common.py
+++ apnatime_backend/settings/common.py
@@ -45,6 +45,7 @@ AUTH_USER_MODEL = 'custom_auth.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
